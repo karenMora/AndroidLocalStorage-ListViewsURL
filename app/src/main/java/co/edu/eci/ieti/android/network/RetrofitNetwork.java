2cb94df921cf57adffec9b81be04bb9761cb9396
karenMora <karen.mora@mail.escuelaing.edu.co>
2019-12-10 16:46:09 -0500

    proyecto

---
+++ app/src/main/java/co/edu/eci/ieti/android/network/RetrofitNetwork.java
@@ -0,0 +1,63 @@
+package co.edu.eci.ieti.android.network;
+
+import co.edu.eci.ieti.android.network.service.AuthService;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import java.io.IOException;
+
+import co.edu.eci.ieti.android.network.service.TaskService;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * @author Santiago Carrillo
+ * 4/23/19.
+ */
+public class RetrofitNetwork
+{
+
+    private AuthService authService;
+    private TaskService taskService;
+
+
+    public RetrofitNetwork()
+    {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl( "http:/10.0.2.2:8080/" ) //localhost for emulator
+                .addConverterFactory( GsonConverterFactory.create() ).build();
+
+        authService = retrofit.create( AuthService.class );
+        taskService=retrofit.create(TaskService.class);
+    }
+    public RetrofitNetwork( final String token )
+    {
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor( new Interceptor()
+        {
+            @Override
+            public okhttp3.Response intercept( Chain chain )
+                    throws IOException
+            {
+                Request original = chain.request();
+
+                Request request = original.newBuilder().header( "Accept", "application/json" ).header( "Authorization",
+                        "Bearer "
+                                + token ).method(
+                        original.method(), original.body() ).build();
+                return chain.proceed( request );
+            }
+        } );
+        Retrofit retrofit =
+                new Retrofit.Builder().baseUrl( "http:/10.0.2.2:8080/" ).addConverterFactory( GsonConverterFactory.create() ).client(
+                        httpClient.build() ).build();
+    }
+
+    public AuthService getAuthService()
+    {
+        return authService;
+    }
+    public TaskService getTaskService()
+    {
+        return taskService;
+    }
+}
---
+++ app/src/main/java/co/edu/eci/ieti/android/network/data/Task.java
@@ -0,0 +1,69 @@
+package co.edu.eci.ieti.android.network.data;
+
+import androidx.annotation.NonNull;
+
+import java.util.Date;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+
+@Entity(tableName = "task_table")
+public class Task {
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "id")
+    private long id;
+
+    @ColumnInfo(name = "description")
+    private String description;
+
+    @ColumnInfo(name = "priority")
+    private int priority;
+
+    @ColumnInfo(name = "assignedTo")
+    private User assignedTo;
+    @ColumnInfo(name = "dueDate")
+    private Date dueDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public User getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(User assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+}
+
